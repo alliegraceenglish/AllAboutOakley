83ed9b406daeb89dc11800c0f1126390e0ad5208
alliegraceenglish <alliegraceenglish@gmail.com>
2019-03-13 19:51:56 -0400

    Create Random Oak picker, add JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,51 @@
-$( document.body ).click(function() {
-$( "#title" ).fadeIn( "slow" );
+//Fade in quote
+$(document).ready(function () {
+    $('#quote').fadeIn(3000).removeClass('hidden');
 });
 
-//var slideIndex = 0;
-//showSlides();
+//Random Oakley Selector
 
-//function showSlides() {
-  //var i;
-  //var slides = document.getElementsByClassName("mySlides");
-  //for (i = 0; i < slides.length; i++) {
-  //  slides[i].style.display = "none";
-  //}
-  //slideIndex++;
-  //if (slideIndex > slides.length) {slideIndex = 1}
-  //slides[slideIndex-1].style.display = "block";
-  //setTimeout(showSlides, 2000); // Change image every 2 seconds
-//}
+let randomOak = '';
+function randomOakResponse() { // Function generates a random number and then assigns a response to randomOak based on that number
+    let randomNumber = Math.floor(Math.random() * 10);
+    switch (randomNumber) {
+        case 0:
+          randomOak = "<img class='randompic' src ='images/profile.jpg'>";
+            break;
+        case 1:
+          randomOak = "<img class='randompic' src ='images/oak1.jpg'>";
+            break;
+        case 2:
+            randomOak = "<img class='randompic' src ='images/oak2.jpg'>";
+            break;
+        case 3:
+            randomOak = "<img class='randompic' src ='images/oak3.jpg'>";
+            break;
+        case 4:
+            randomOak = "<img class='randompic' src ='images/oak4.jpg'>";
+            break;
+        case 5:
+            randomOak = "<img class='randompic' src ='images/oak5.jpg'>";
+            break;
+        case 6:
+            randomOak = "<img class='randompic' src ='images/oak6.jpg'>";
+            break;
+        case 7:
+            randomOak = "<img class='randompic' src ='images/oak7.jpg'>";
+            break;
+        case 8:
+            randomOak = "<img class='randompic' src ='images/oak8.jpg'>";
+            break;
+        case 9:
+            randomOak = "<img class='randompic' src ='images/oak9.jpg'>";
+            break;
+        default:
+            randomOak = "<img class='randompic' src ='images/profile.jpg'>";
+    }
+    return randomOak;
+}
+function pick () {
+    randomOakResponse();
+    document.getElementById('randompic').innerHTML = randomOak;
+}
+document.getElementById('clickMe').addEventListener('click', pick);
